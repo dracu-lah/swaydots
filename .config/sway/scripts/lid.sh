b5c67aeeff03fc004b1579cab7b518fdd0e0c5a1
dracu-lah <nevilnicks4321@gmail.com>
2026-05-05 16:49:12 +0530

    fix:updated changes

--- .config/sway/scripts/lid.sh
+++ .config/sway/scripts/lid.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-# Lid close: suspend if on battery, blank display if on AC.
+# Lid close: hibernate.
 
-ac_online() {
-    for f in /sys/class/power_supply/*/online; do
-        [ -e "$f" ] || continue
-        t="${f%online}type"
-        [ -e "$t" ] && [ "$(cat "$t")" = "Mains" ] && [ "$(cat "$f")" = "1" ] && return 0
-    done
-    return 1
-}
-
-if ac_online; then
-    swaymsg 'output eDP-1 disable'
-else
-    systemctl suspend
-fi
+systemctl hibernate
